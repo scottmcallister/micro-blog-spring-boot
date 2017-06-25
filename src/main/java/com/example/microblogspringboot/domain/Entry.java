8244ac7509aac1b0407abee9e8cbcb6cc86ec7aa
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-06-24 22:21:55 -0400

    create entity for blog entry

---
+++ src/main/java/com/example/microblogspringboot/domain/Entry.java
@@ -0,0 +1,41 @@
+package com.example.microblogspringboot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by scottmcallister on 2017-06-24.
+ */
+@Entity
+public class Entry {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String content;
+
+    protected Entry() {}
+
+    public Entry(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/example/microblogspringboot/domain/EntryRepository.java
@@ -0,0 +1,13 @@
+package com.example.microblogspringboot.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by scottmcallister on 2017-06-24.
+ */
+public interface EntryRepository extends CrudRepository<Entry, Long> {
+    List<Entry> findAll();
+    List<Entry> findByTitle(String title);
+}
